335803a532eb2ed9bb1e4ab9f35ad68c25968d8e
COLCO <mario@hashdog.com>
2020-04-06 21:31:42 -0300

    added bitpay-rates module

---
+++ bitpay-rates.d.ts
@@ -0,0 +1,1 @@
+declare module 'bitpay-rates';
--- pages/api/rates.ts
+++ pages/api/rates.ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import bitpayRates from 'bitpay-rates'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { name = 'API' } = req.query
-  res.status(200).json({ name })
+  const { code } = req.query
+  bitpayRates.get(code)
+    .then(rates => res.status(200).json(rates))
+    .catch(err => {
+      console.log('ERROR', err)
+      res.status(500).json({ message: 'Internal error' })
+    })
 }
